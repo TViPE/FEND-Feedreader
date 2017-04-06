b980c5c162e80953b78c5a4734d9c84e3d6211db
TRI VI <trivi2710@gmail.com>
2017-04-06 11:55:01 -0400

    init specs for RSS Feeds suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,16 +31,31 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('has a URL defined and that the URL is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                var feedUrl = feed.url;
+                expect(feedUrl).toBeDefined();
+                expect(feedUrl).not.toBeNull();
+            })
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has a name defined and that the name is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                var feedName = feed.name;
+                expect(feedName).toBeDefined();
+                expect(feedName).not.toBeNull();
+            })
+         })
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function) {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -53,7 +68,7 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+    }
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
